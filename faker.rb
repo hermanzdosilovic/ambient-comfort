12656429bcbc3e9ba7bd51d9a71c304a24caee46
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2017-06-05 14:15:47 +0200

    Get API_KEY from environment in faker.rb

--- faker.rb
+++ faker.rb
@@ -4,13 +4,13 @@ require 'uri'
 require 'json'
 
 SLEEP_TIME = 2
-AUTH_TOKEN = ''
+API_KEY = ENV['API_KEY'].to_s
 
 url = ARGV[0].to_s.empty? ? 'http://localhost:3000' : ARGV[0]
 uri = URI.parse(url + '/logs')
 header = {
     'Content-Type': 'application/json',
-    'X-Auth-Token': AUTH_TOKEN
+    'X-Auth-Token': API_KEY
 }
 
 puts "Sending fake data to #{uri} ..."
@@ -24,6 +24,7 @@ while true do
         noise: Faker::Number.between(0, 5).to_i + Faker::Number.decimal(0, 5).to_f
     }
     http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true if uri.scheme == 'https'
     request = Net::HTTP::Post.new(uri.request_uri, header)
     request.body = log.to_json
     response = http.request(request)
