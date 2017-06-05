64a2f36a622a1d2e5fe1ecfefeb97651663ca6f3
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2017-06-05 21:30:53 +0200

    Updated screenshots

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   def root
     @logs = Log.all.order(created_at: :desc).paginate(page: params[:page], per_page: 30)
     @latest_logs = Log.where('logs.created_at >= ?', DateTime.now.yesterday).order(created_at: :desc)
-    @message = Log.last.message
+    @message = Log.last.try(:message)
     respond_to do |format|
       format.html
       format.js
